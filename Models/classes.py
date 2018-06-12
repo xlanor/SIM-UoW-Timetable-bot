11cf7e3543c86e02f81b76fea880a9f15e1367f4
Jingkai <tanjingkai@me.com>
2018-06-12 15:59:13 +0800

    Fixed to comply with PEP-8

--- Models/classes.py
+++ Models/classes.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##
 #   Copyright (C) 2018 JING KAI TAN
 #
@@ -15,7 +15,7 @@
 #
 #   You should have received a copy of the GNU Affero General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# 
+#
 ##
 #   Model to hold class time.
 #   Written by xlanor
@@ -25,34 +25,72 @@ import time as clock
 from bs4 import BeautifulSoup
 import re
 
+
 class IndividualClassStructure():
-    def __init__(self,name):
+    def __init__(self, name):
         self.__name = name
         # Initialize a few empty holding vars.
         self.__date = ""
         self.__starttime = ""
         self.__endtime = ""
         self.__location = ""
-        self.__class_type= ""
+        self.__class_type = ""
     """
         Gets the variables in a dictionary format.
     """
     def get_dict(self):
-        return {"class_name":self.__name,"date":self.__date,"Start_Time":self.__starttime,"End_Time":self.__endtime,"Location":self.__location,"Type":self.__class_type}
+        return_dict = {
+                        "class_name": self.__name,
+                        "date": self.__date,
+                        "Start_Time": self.__starttime,
+                        "End_Time": self.__endtime,
+                        "Location": self.__location,
+                        "Type": self.__class_type
+                        }
+        return return_dict
+
+    def get_name(self):
+        return self.__name
+
+    def get_date(self):
+        return self.__date
+
+    def get_loc(self):
+        return self.__location
+
+    def get_start_time(self):
+        return self.__starttime
+
+    def get_end_time(self):
+        return self.__endtime
+
+    def get_class_type(self):
+        return self.__class_type
+
     """
         Gets the dates of classes, strips it and reformats it.
         @params row, a row pulled from the html
     """
-    def set_date_of_class(self,row):
-        getdate = row.find("span",{'id':re.compile(r'(MTG_DATES\$)([0-9]{1})')})
+    def set_date_of_class(self, row):
+        getdate = row.find("span",
+                           {
+                              'id': re.compile(r'(MTG_DATES\$)([0-9]{1})')
+                           }
+                           )
         if not getdate.text.strip():
-            getdate = row.find("span",{'id':re.compile(r'(MTG_DATES\$)([0-9]{1})([0-9]{1})')})
-        
-        #	date is returned in the format DD/MM/YYYY - DD/MM/YYYY
-        #	because the end date is redundant (unless SIM decides to hold 24hr overnight classes)
-        
-        date = getdate.text 
-        strippeddate = (((date.strip()).split("-"))[0]).strip() #strips spaces, splits by -, returns first date.
+            getdate = row.find("span",
+                               {
+                                  'id': re.compile(r'(MTG_DATES\$)([0-9]{1})([0-9]{1})')  # noqa
+                               }
+                               )
+
+        # date is returned in the format DD/MM/YYYY - DD/MM/YYYY
+        # because the end date is redundant
+        # (unless SIM decides to hold 24hr overnight classes)
+
+        date = getdate.text
+        # strips spaces, splits by -, returns first date.
+        strippeddate = (((date.strip()).split("-"))[0]).strip()
 
         self.__date = strippeddate
 
@@ -62,20 +100,30 @@ class IndividualClassStructure():
         @params row, a row object pulled from the html.
 
     """
-    def set_time(self,class_type,row):
+    def set_time(self, class_type, row):
         if class_type:
-            self.__starttime = "{} {}".format(self.__date,self.__get_time(class_type,row))
+            self.__starttime = "{} {}".format(self.__date,
+                                              self.__get_time(class_type, row))
         else:
-            self.__endtime = "{} {}".format(self.__date,self.__get_time(class_type,row))
+            self.__endtime = "{} {}".format(self.__date,
+                                            self.__get_time(class_type, row))
 
     """
         Sets the location of the class object
         @params row, the row scraped from the html.
     """
-    def set_location(self,row):
-        getlocation = row.find("span",{'id':re.compile(r'(MTG_LOC\$)([0-9]{1})')})
+    def set_location(self, row):
+        getlocation = row.find("span",
+                               {
+                                   'id': re.compile(r'(MTG_LOC\$)([0-9]{1})')
+                               }
+                               )
         if not getlocation.text.strip():
-            getlocation = row.find("span",{'id':re.compile(r'(MTG_LOC\$)([0-9]{1})([0-9]{1})')})
+            getlocation = row.find("span",
+                                   {
+                                       'id': re.compile(r'(MTG_LOC\$)([0-9]{1})([0-9]{1})')  # noqa
+                                   }
+                                   )
 
         self.__location = getlocation.text
 
@@ -87,26 +135,27 @@ class IndividualClassStructure():
         @params rowid, the current rowid.
     """
 
-    def determine_class_type(self,list_of_class_type_keys,class_type_dict,no_of_keys,rowid):
+    def determine_class_type(self, list_of_class_type_keys,
+                             class_type_dict, no_of_keys, rowid):
         counter = 0
         trigger = True
-        while trigger :
+        while trigger:
             actual_class_type = ""
             if int(rowid) >= int(list_of_class_type_keys[counter]):
-                actual_class_type = class_type_dict[list_of_class_type_keys[counter]]
+                actual_class_type = class_type_dict[list_of_class_type_keys[counter]]  # noqa
                 counter += 1
                 if counter >= (no_of_keys):
-                    trigger = False			
+                    trigger = False
             else:
                 trigger = False
         # Fixes SIMConnect's spelling error.
         if actual_class_type == "Consultati":
-            actual_class_type = "Consultation" 
+            actual_class_type = "Consultation"
         self.__class_type = actual_class_type
-        
+
     """################################################
-                
-                BEGIN PRIVATE METHODS        
+
+                BEGIN PRIVATE METHODS
 
     ################################################"""
 
@@ -115,13 +164,25 @@ class IndividualClassStructure():
         @params type, true if start time else end time.
         @params row, a row object pulled from the html.
     """
-    def __get_time(self,type,row):
-        gettime = row.find("span",{'id':re.compile(r'(MTG_SCHED\$)([0-9]{1})')})
+    def __get_time(self, type, row):
+        gettime = row.find("span",
+                           {
+                               'id': re.compile(r'(MTG_SCHED\$)([0-9]{1})')
+                           }
+                           )
         if not gettime.text.strip():
-            gettime = row.find("span",{'id':re.compile(r'(MTG_SCHED\$)([0-9]{1})([0-9]{1})')})
+            gettime = row.find("span",
+                               {
+                                   'id': re.compile(r'(MTG_SCHED\$)([0-9]{1})([0-9]{1})')  # noqa
+                               }
+                               )
         time = gettime.text
-        #	time is returned as a value of DD<space>STARTTIME<AM/PM><space>-<space>ENDTIME<AM/PM>
-        #	removes space, removes DD,splits by -. This forms a list in the format [XXXXAM, YYYYAM] Where x is start, y is end
+        # time is returned as a value of
+        # DD<space>STARTTIME<AM/PM><space>-<space>ENDTIME<AM/PM>
+        # removes space, removes DD,splits by -.
+        # This forms a list in the format
+        # [XXXXAM, YYYYAM]
+        # Where x is start, y is end
         strippedtime = (time.strip()[2:]).split("-")
         time = (strippedtime[0]).strip() if type else (strippedtime[1]).strip()
         time = self.__format_time(time)
@@ -131,9 +192,7 @@ class IndividualClassStructure():
         Concantates a 0 if time is < 7 chars.
         @params time, time to modify.
     """
-    def __format_time(self,time):
+    def __format_time(self, time):
         if len(time) < 7:
             time = "0"+time
         return time
-
-    
--- simconnect_ripper.py
+++ simconnect_ripper.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##
 #   Copyright (C) 2018 JING KAI TAN
 #
@@ -15,32 +15,38 @@
 #
 #   You should have received a copy of the GNU Affero General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# 
+#
 ##
 #   Cronus module to rip the timetable from SIM connect
-#   Using PhantomJS as a headless browser, and selenium | bs4 to navigate and scrape the results.
+#   Using PhantomJS as a headless browser, and selenium or
+#   bs4 to navigate and scrape the results.
 ##
 import time as clock
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from contextlib import closing
 from bs4 import BeautifulSoup
-import re, json,sys
+import re
+import json
+import sys
 from Models.classes import IndividualClassStructure
 
 """
     Superclass definition.
 """
+
+
 class SIMConnect():
     """
         Constructor
         @params username, the username of the client
         @params password, the decrypted password of the client
     """
-    def __init__(self,username,password):
-        self.__login_url = "https://simconnect.simge.edu.sg/psp/paprd/EMPLOYEE/HRMS/s/WEBLIB_EOPPB.ISCRIPT1.FieldFormula.Iscript_SM_Redirect?cmd=login"
-        self.timetable_page = "https://simconnect1.simge.edu.sg:444/psc/csprd_2/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_LIST.GBL"
-        self.logout_url = "https://simconnect.simge.edu.sg/psp/paprd/EMPLOYEE/EMPL/?cmd=logout"
+    def __init__(self, username, password):
+        # noqa is used here to prevent breaking of PEP-8
+        self.__login_url = "https://simconnect.simge.edu.sg/psp/paprd/EMPLOYEE/HRMS/s/WEBLIB_EOPPB.ISCRIPT1.FieldFormula.Iscript_SM_Redirect?cmd=login"  # noqa
+        self.timetable_page = "https://simconnect1.simge.edu.sg:444/psc/csprd_2/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_LIST.GBL"  # noqa
+        self.logout_url = "https://simconnect.simge.edu.sg/psp/paprd/EMPLOYEE/EMPL/?cmd=logout"  # noqa
         self.username = username
         self.password = password
     """
@@ -48,35 +54,35 @@ class SIMConnect():
         @params driver, an initalized selenium driver.
         @return string, the whole page source.
     """
-    def login(self,driver):
-        
+    def login(self, driver):
         driver.get(self.__login_url)
         # we use sleep to let the page happily load throughout the script.
-        clock.sleep(5) 
+        clock.sleep(5)
         # finds userinput box
         usr = driver.find_element_by_name('userid')
         # finds password box
         passw = driver.find_element_by_name('pwd')
         # finds login button
-        logbtn = driver.find_element_by_name('Submit') 
+        logbtn = driver.find_element_by_name('Submit')
         # sends the keys to the driver
         usr.send_keys(self.username)
         passw.send_keys(self.password)
-        #clicks the button
+        # clicks the button
         logbtn.click()
         clock.sleep(6)
-        #print (driver.page_source)
-        #driver.save_screenshot('test.png')
+        # print (driver.page_source)
+        # driver.save_screenshot('test.png')
         return driver.page_source
-    """ 
-        Default execution method that checks for login 
+    """
+        Default execution method that checks for login
         @return boolean variable , True if able to login else false.
     """
     def execute(self):
-        #"API" that populates timetable. It was never designed for this purpose, but we're going to use it
-        #initialize PhantomJS, define settings
-        driver = webdriver.PhantomJS(executable_path='./phantomjs',service_args=['--ignore-ssl-errors=true', '--ssl-protocol=TLSv1'])
-        driver.set_window_size(1124, 850) 
+        # "API" that populates timetable. It was never designed
+        # for this purpose, but we're going to use it
+        # initialize PhantomJS, define settings
+        driver = webdriver.PhantomJS(executable_path='./phantomjs', service_args=['--ignore-ssl-errors=true', '--ssl-protocol=TLSv1'])  # noqa
+        driver.set_window_size(1124, 850)
         page_source = self.login(driver)
         if self.logout_url in page_source:
             driver.close()
@@ -87,21 +93,24 @@ class SIMConnect():
 """
     RipTimeTable inherits SIMConnect.
 """
+
+
 class RipTimeTable(SIMConnect):
     """
         Constructor, calls superclass
         @params username, the username of the client
         @params password, the decrypted password of the client
     """
-    def __init__(self,username,password):
-        super(RipTimeTable, self).__init__(username,password)
+    def __init__(self, username, password):
+        super(RipTimeTable, self).__init__(username, password)
     """
-        Navigates to the latest timetable in the event that there is more than 1 timetable.
+        Navigates to the latest timetable in the event
+        that there is more than 1 timetable.
         @params driver, an initalized selenium dirver
         @params latest_term, the latest term that you want to pull.
         @return soup, a formated beautiful soup object.
     """
-    def navigate_to_latest_timetable(self,driver,latest_term):
+    def navigate_to_latest_timetable(self, driver, latest_term):
         # constructing the latest term's id
         newid = 'SSR_DUMMY_RECV1$sels$'+str(latest_term)+'$$0'
 
@@ -110,15 +119,17 @@ class RipTimeTable(SIMConnect):
         term_button.click()
 
         # more hunting for buttons.
-        continue_button = driver.find_element_by_id('DERIVED_SSS_SCT_SSR_PB_GO')
+        continue_button = driver.find_element_by_id(
+                                                    'DERIVED_SSS_SCT_SSR_PB_GO'
+                                                    )
         continue_button.click()
-        
+
         # loading the new timetablepage...
         clock.sleep(5)
-        
+
         # grab the source, format it, return to bs4
         new_formatted_result = driver.page_source
-        soup = BeautifulSoup(new_formatted_result,"html.parser")
+        soup = BeautifulSoup(new_formatted_result, "html.parser")
 
         return soup
 
@@ -127,10 +138,18 @@ class RipTimeTable(SIMConnect):
         @params row, a row pulled from the thml
         @params class_type_dict, the dictionary to store the class type.
     """
-    def __type_of_classes(self,row,class_type_dict):
-        gettype = row.find("span",{"id":re.compile(r'(MTG_COMP\$)([0-9]{1})')})
+    def __type_of_classes(self, row, class_type_dict):
+        gettype = row.find("span",
+                           {
+                                "id": re.compile(r'(MTG_COMP\$)([0-9]{1})')
+                           }
+                           )
         if not gettype.text:
-            gettype = row.find("span",{"id":re.compile(r'(MTG_COMP\$)([0-9]{1})([0-9]{1})')})
+            gettype = row.find("span",
+                               {
+                                   "id": re.compile(r'(MTG_COMP\$)([0-9]{1})([0-9]{1})')  # noqa
+                               }
+                               )
 
         if gettype.text.strip():
             type_class = gettype.text
@@ -138,8 +157,8 @@ class RipTimeTable(SIMConnect):
             '''#########################################################
                         Only the first row for each type has
                             the value of the type.
-                    ie: row 0, first lecture type will contain 
-                lecture., then its blank until row 15, row 15 contains 
+                    ie: row 0, first lecture type will contain
+                lecture., then its blank until row 15, row 15 contains
                 tutorial. We put it into a dict to compare later.
             #########################################################'''
             class_type_dict[type_id] = type_class
@@ -149,46 +168,62 @@ class RipTimeTable(SIMConnect):
         @params row, a row pulled from the HTML
         @return rowid, the current row id.
     """
-    def __get_row_id(self,row):
-        getlocation = row.find("span",{'id':re.compile(r'(MTG_LOC\$)([0-9]{1})')})
+    def __get_row_id(self, row):
+        getlocation = row.find("span",
+                               {
+                                    'id': re.compile(r'(MTG_LOC\$)([0-9]{1})')
+                               }
+                               )
         if not getlocation.text.strip():
-            getlocation = row.find("span",{'id':re.compile(r'(MTG_LOC\$)([0-9]{1})([0-9]{1})')})
+            getlocation = row.find("span",
+                                   {
+                                       'id': re.compile(r'(MTG_LOC\$)([0-9]{1})([0-9]{1})')  # noqa
+                                   }
+                                   )
 
         rowid = (((getlocation.get('id')).split("$"))[1]).strip()
         return rowid
 
-    def handle_single_digit_row(self,singledigittablerow,class_type_dict,list_of_results,subjectitlename):
+    def handle_single_digit_row(self, singledigittablerow,
+                                class_type_dict, list_of_results,
+                                subjectitlename):
         for row in singledigittablerow:
             # Instantiate a  new class object.
             class_object = IndividualClassStructure(subjectitlename)
             '''####################################################################################
                                     Here we're going to use regex.
-                            Why do we repeat same regex twice with variance?
+                              Why do we repeat same regex twice with variance?
                             First we check for id with regex for single digit
-                                    For example, MTG_DATES$0
-                    If in that div, MTGDATES$# WHERE # is a random number does not match regex, 
-                        It means that we're in a row where MTGDATES$ is MTGDATES$##
+                                     For example, MTG_DATES$0
+                                  If in that div, MTGDATES$# WHERE # is a
+                                    random number does not match regex,
+                                    It means that we're in a row where
+                                        MTGDATES$ is MTGDATES$##
                                 Thus we write another regex for that row.
-                This was probably never meant to be pulled as an API that's why its so fucked up.
-            ####################################################################################'''
+                                  This was probably never meant to be
+                              pulled as an API that's why its so fucked up.
+            ####################################################################################'''  # noqa
 
-            self.__type_of_classes(row,class_type_dict)
+            self.__type_of_classes(row, class_type_dict)
 
             # Sets the date of the class in the class object.
             class_object.set_date_of_class(row)
             # Sets the start time of the class
-            class_object.set_time(True,row)
+            class_object.set_time(True, row)
             # Sets the end time of the class
-            class_object.set_time(False,row)
+            class_object.set_time(False, row)
             # Sets the location of the class (where the class is being held.)
             class_object.set_location(row)
-            # Time to determine class type (lecture, tutorial) from the array above.
-            rowid = self.__get_row_id(row)          
+            # Time to determine class type from the array above.
+            rowid = self.__get_row_id(row)
             list_of_class_type_keys = list(class_type_dict.keys())
             list_of_class_type_keys = sorted(list_of_class_type_keys)
             no_of_keys = len(list_of_class_type_keys)
 
-            class_object.determine_class_type(list_of_class_type_keys,class_type_dict,no_of_keys,rowid)
+            class_object.determine_class_type(list_of_class_type_keys,
+                                              class_type_dict, no_of_keys,
+                                              rowid
+                                              )
 
             list_of_results.append(class_object)
     """
@@ -197,29 +232,48 @@ class RipTimeTable(SIMConnect):
         @ params class_type_dict, an empty dictionary.
         @ params list_of_results, an empty list.
     """
-    def process_subject_div(self,subjectdiv,class_type_dict,list_of_results):
+    def process_subject_div(self, subjectdiv,
+                            class_type_dict, list_of_results):
         for div in subjectdiv:
             class_type_dict.clear()
-            subjectitle = div.find("td",{'class':'PAGROUPDIVIDER'})
+            subjectitle = div.find("td",
+                                   {
+                                      'class': 'PAGROUPDIVIDER'
+                                   }
+                                   )
             subjectitlename = subjectitle.text
-            singledigittablerow = div.findAll('tr',{'id':re.compile(r'(trCLASS_MTG_VW\$)([0-9]{1})(_row)([0-9]{1})')})
-            self.handle_single_digit_row(singledigittablerow,class_type_dict,list_of_results,subjectitlename)
+            singledigittablerow = div.findAll('tr',
+                                                {
+                                                    'id': re.compile(r'(trCLASS_MTG_VW\$)([0-9]{1})(_row)([0-9]{1})')  # noqa
+                                                }
+                                                )
+            self.handle_single_digit_row(singledigittablerow,
+                                         class_type_dict, list_of_results,
+                                         subjectitlename
+                                         )
     """
         An override method for the super class.
-    """        
+    """
     def execute(self):
-        #"API" that populates timetable. It was never designed for this purpose, but we're going to use it
-        #initialize PhantomJS, define settings
-        driver = webdriver.PhantomJS(executable_path='./phantomjs',service_args=['--ignore-ssl-errors=true', '--ssl-protocol=TLSv1'])
-        driver.set_window_size(1124, 850) 
+        # "API" that populates timetable.
+        # It was never designed for this purpose,
+        # but we're going to use it
+        # initialize PhantomJS, define settings
+        driver = webdriver.PhantomJS(executable_path='./phantomjs', service_args=['--ignore-ssl-errors=true', '--ssl-protocol=TLSv1'])  # noqa
+        driver.set_window_size(1124, 850)
         page_source = self.login(driver)
         if self.logout_url in page_source:
-            driver.get(self.timetable_page) 
+            driver.get(self.timetable_page)
             formatted_result = driver.page_source
-            soup = BeautifulSoup(formatted_result,"html.parser")
-            termdiv = soup.findAll('span',{'id':re.compile(r'(TERM_CAR\$)([0-9]{1})')})
+            soup = BeautifulSoup(formatted_result, "html.parser")
+            termdiv = soup.findAll('span',
+                                   {
+                                       'id': re.compile(r'(TERM_CAR\$)([0-9]{1})')  # noqa
+                                   }
+                                   )
 
-            # If there is more than 1 timetable avaliable, we default to the latest term's timetable.
+            # If there is more than 1 timetable avaliable,
+            # we default to the latest term's timetable.
             if len(termdiv) != 0:
                 latest_term = len(termdiv)-1
                 soup = self.navigate_to_latest_timetable(driver,latest_term)
